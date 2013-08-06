6a6af8b1909749e3a87c9c231bc4d9373c0b3d32
Alexander Shabalin <shabalyn.a@gmail.com>
2013-08-07 00:23:57 +0400

    Generated type variables are better now

--- src/KWInference.hs
+++ src/KWInference.hs
@@ -1,11 +1,30 @@
 module KWInference(TypeError(..), inferType) where
 
+import Control.Applicative
+import Data.Char
+import Data.List
+
 import Term
 import Type
 import ThetaReduction
 import ASUP
 import TypeError
 
+generateSubs :: [String] -> [String] -> [Substitution Type]
+generateSubs taken = go "a"
+ where go _ [] = []
+       go x (v:vs)
+        | x `elem` taken = go (next x) (v:vs)
+        | otherwise = (v `AssignTo` TVar x) : go (next x) vs
+       next x
+        | last x == 'z' = replicate (length x + 1) 'a'
+        | otherwise = init x ++ [chr (ord (last x) + 1)]
+
+prettifyType :: [(String, Type)] -> Type -> Type
+prettifyType userTypes t = substituteAll subs t
+ where subs = generateSubs ftvUser (freeVars t \\ ftvUser)
+       ftvUser = foldr union [] $ map (freeVars . snd) userTypes
+
 inferType :: [(String, Type)] -> Term -> TypeErrorMonad Type
 inferType userTypes term = do
-   typeInASUP userTypes $ thetaReduction term
+   prettifyType userTypes <$> typeInASUP userTypes (thetaReduction term)
